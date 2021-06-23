469e800ce9bb63d64f79f121a33f5a8e7049efd0
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-23 19:52:16 +0530

    ADDED: program to find even or odd number

---
+++ src/com/bridgelabz/basic_core_program/EvenOrOdd/EvenOdd.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.basic_core_program.EvenOrOdd;
+
+
+import java.util.Scanner;
+
+public class EvenOdd
+{
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+   
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
